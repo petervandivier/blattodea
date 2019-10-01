b7ca088e20dc29f35a8fed7232cfd5c38826853f
petervandivier <petervandivier@gmail.com>
2019-10-01 16:13:18 +0100

    laptop UI access + launch-on-finish

--- make-all.ps1
+++ make-all.ps1
@@ -5,3 +5,9 @@
 . make/loadbalancer
 . make/certs
 . make/initdb
+
+$script:IP = (Get-Content ./conf/actual/cluster.json | ConvertFrom-Json)[0].Instances.PublicIPAddress
+# $browser = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome' "https://$IP`:8080"
+# TODO: open async
+# Start-Process $browser "https://$IP`:8080"
+open -a "Google Chrome" "https://$IP`:8080" 
