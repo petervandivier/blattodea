dc81e76bc9be0baa6818d9739ea597425ca06cce
petervandivier <petervandivier@gmail.com>
2019-10-07 09:08:45 +0100

    Reset-Ingress - add named user * MBP changes hostname :expressionless:

--- functions/Reset-CrdbAdminIngress.ps1
+++ functions/Reset-CrdbAdminIngress.ps1
@@ -1,4 +1,4 @@
-function Reset-MyCrdbIngress {
+function Reset-CrdbAdminIngress {
 <#
 .DESCRIPTION
     one-liner for re-granting access to yourself when you change wifi networks
@@ -8,17 +8,16 @@ function Reset-MyCrdbIngress {
     does removing & re-granting these rules bork the cluster? i assume no, but it's probably prudent to check at some point
 #>
     [cmdletbinding()]Param(
-        [string]$AWSRegion = $StoredAWSRegion
+        [string]$user = "$(whoami)@$(hostname)"
     )
 
-    $me = "$(whoami)@$(hostname)"
     $sg = Get-EC2SecurityGroup -GroupId (Get-Content ./conf/actual/SecurityGroup.json | ConvertFrom-Json).GroupId
     $my_ip = (Invoke-WebRequest -Uri "http://ifconfig.me/ip" -TimeoutSec 10).Content 
 
     if($my_ip){
         Write-Verbose "IP Address parsed as '$my_ip'. Proceeding to update security group ingress rules for '$($sg.GroupId)'"
 
-        foreach($perm in ($sg.IpPermission | Where-Object {$_.IpV4Ranges.Description -Like "*$me*"})){
+        foreach($perm in ($sg.IpPermission | Where-Object {$_.IpV4Ranges.Description -like "*$user*"})){
             Revoke-EC2SecurityGroupIngress -GroupId $sg.GroupId -IpPermission $perm
             $perm.Ipv4Ranges | ForEach-Object {$_.CidrIp = "$my_ip/32"}
             Grant-EC2SecurityGroupIngress -GroupId $sg.GroupId -IpPermission $perm
