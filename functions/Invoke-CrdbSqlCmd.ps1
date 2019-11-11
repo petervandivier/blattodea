c2a05f6c347120d67eabd0b57ac98a82d61a2e48
petervandivier <petervandivier@gmail.com>
2019-11-11 21:51:49 +0000

    Invoke-CrdbSqlCmd -File

--- functions/Invoke-CrdbSqlCmd.ps1
+++ functions/Invoke-CrdbSqlCmd.ps1
@@ -1,9 +1,20 @@
 function Invoke-CrdbSqlCmd {
+<#
+.DESCRIPTION
+    Same behaviour as psql - accepts both command and file inputs and executes command first
+
+.EXAMPLE
+
+
+#>
     [CmdletBinding()]
     param (
         [Parameter(ValueFromPipeline=$true,Position=0)]
         [String]
         $Command,
+        [Parameter()]
+        [String]
+        $File,
         [Parameter()]
         [String]
         $SqlHost = ((Get-Content "./conf/actual/Cluster.Default.json" | ConvertFrom-Json).Instances[0].PublicIpAddress),
@@ -19,7 +30,11 @@ function Invoke-CrdbSqlCmd {
         $certsDir = (Resolve-Path "$($btd_Defaults.CertsDirectory)/certs")
     )
 
-    Write-Output $Command | cockroach sql --certs-dir=$certsDir --host=$SqlHost --database=$Database
+    if($Command){Write-Output $Command | cockroach sql --certs-dir=$certsDir --host=$SqlHost --database=$Database}
+    if($File){
+        $Command = Get-Content $File -Raw 
+        if($Command){Write-Output $Command | cockroach sql --certs-dir=$certsDir --host=$SqlHost --database=$Database}
+    }
 }
 
 New-Alias -Name 'icr' -Value 'Invoke-CrdbSqlCmd' -Force
